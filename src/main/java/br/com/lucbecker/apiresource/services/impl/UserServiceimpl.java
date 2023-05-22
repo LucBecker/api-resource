bd7e5235b65b8686459af0b0fcf2d94a745a16c6
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-21 23:08:31 -0300

    tratamento de excecao para objeto nao encontrado

--- src/main/java/br/com/lucbecker/apiresource/services/impl/UserServiceimpl.java
+++ src/main/java/br/com/lucbecker/apiresource/services/impl/UserServiceimpl.java
@@ -3,6 +3,7 @@ package br.com.lucbecker.apiresource.services.impl;
 import br.com.lucbecker.apiresource.domain.User;
 import br.com.lucbecker.apiresource.repositories.UserRepository;
 import br.com.lucbecker.apiresource.services.UserService;
+import br.com.lucbecker.apiresource.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,6 @@ public class UserServiceimpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado "));
     }
 }

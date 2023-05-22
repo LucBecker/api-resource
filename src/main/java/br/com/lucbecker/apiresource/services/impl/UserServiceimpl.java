e854b38c097bdb2481cc7ce472a2ba0753f4ed39
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-21 21:57:32 -0300

    classe user service impl findById

---
+++ src/main/java/br/com/lucbecker/apiresource/services/impl/UserServiceimpl.java
@@ -0,0 +1,23 @@
+package br.com.lucbecker.apiresource.services.impl;
+
+import br.com.lucbecker.apiresource.domain.User;
+import br.com.lucbecker.apiresource.repositories.UserRepository;
+import br.com.lucbecker.apiresource.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceimpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}

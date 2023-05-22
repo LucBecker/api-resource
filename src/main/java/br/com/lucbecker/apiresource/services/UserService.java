90faa22209ab334b14fbb5ecc513c7db13000207
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-22 09:56:03 -0300

    endpoint findAll para user

--- src/main/java/br/com/lucbecker/apiresource/services/UserService.java
+++ src/main/java/br/com/lucbecker/apiresource/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.lucbecker.apiresource.services;
 
 import br.com.lucbecker.apiresource.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }

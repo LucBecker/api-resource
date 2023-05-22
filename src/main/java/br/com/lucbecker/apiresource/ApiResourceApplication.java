c043f85c733d43c3038f23bb8d0a69fda2ac2347
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-21 21:07:30 -0300

    projeto iniciado

---
+++ src/main/java/br/com/lucbecker/apiresource/ApiResourceApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucbecker.apiresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucbecker/apiresource/ApiResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucbecker.apiresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

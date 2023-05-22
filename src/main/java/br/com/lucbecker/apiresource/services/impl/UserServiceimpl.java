c2b5324e25601a32c16cdfeb87d181cbd725d994
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-22 12:06:12 -0300

    endpoint create para user

--- src/main/java/br/com/lucbecker/apiresource/services/impl/UserServiceimpl.java
+++ src/main/java/br/com/lucbecker/apiresource/services/impl/UserServiceimpl.java
@@ -1,9 +1,11 @@
 package br.com.lucbecker.apiresource.services.impl;
 
 import br.com.lucbecker.apiresource.domain.User;
+import br.com.lucbecker.apiresource.domain.dto.UserDTO;
 import br.com.lucbecker.apiresource.repositories.UserRepository;
 import br.com.lucbecker.apiresource.services.UserService;
 import br.com.lucbecker.apiresource.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class UserServiceimpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
 
     @Override
     public User findById(Integer id) {
@@ -25,5 +30,10 @@ public class UserServiceimpl implements UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return repository.save(mapper.map(obj, User.class));
     }
 }

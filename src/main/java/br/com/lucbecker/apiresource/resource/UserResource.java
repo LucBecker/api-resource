3be10fa2ee2512852115aaa146bc5f68f2353545
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-05-21 21:57:49 -0300

    classe user resource findById

--- src/main/java/br/com/lucbecker/apiresource/resource/UserResource.java
+++ src/main/java/br/com/lucbecker/apiresource/resource/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.lucbecker.apiresource.resource;
 
 import br.com.lucbecker.apiresource.domain.User;
+import br.com.lucbecker.apiresource.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "lucas", "lucas@mail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
